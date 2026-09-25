33c372d213afe1ce7161c2910577d1aaec6b5f31
Bianca <bia.rscr@hotmail.com>
2026-09-25 20:49:35 -0300

    add variavel float

--- dados/python/variaveis/variaveis.py
+++ dados/python/variaveis/variaveis.py
@@ -1,3 +1,23 @@
+# Variaveis e tipos de dados
+
+# Tipos de dados: 
+#   String: texto
+#   Inteiro: número inteiro
+#   Float: número decimal
+#   Booleano: True ou False
+
+nome = 'Bianca'
+idade = 37
+altura = 1.61
+estado = 'Rio de Janeiro'
+profissao = 'Engenheira de Dados'
+tem_cachorro = True
+
+# O que são variáveis?
+# Variáveis em Python são espaços na memória que armazenam valores.
+# Elas são criadas quando atribuímos um valor a uma variável.
+
+# O que são variáveis em python?
 # Variáveis em Python são espaços na memória que armazenam valores.
 # Elas são criadas quando atribuímos um valor a uma variável.
 
@@ -9,17 +29,12 @@
 
 # Regras para nomear variáveis em Python:
 # - Devem começar com uma letra ou underscore (_)
-# - Podem conter letras, números e underscores
-# - São case-sensitive
-# - Não podem conter espaços
-# - Não podem conter caracteres especiais
-# - Não podem ser palavras reservadas
+# - Podem conter letras, números e underscores (Ex: minha_variavel, minhaVariavel, minha_Variavel)
+# - São case-sensitive (as variáveis 'nome', 'Nome' e 'NOME' são diferentes)
+# - Não podem conter espaços (Ex: minha variavel - inválido)
+# - Não podem conter caracteres especiais (exceto underscore) (Ex: minha@variavel - inválido)
+# - Não podem ser palavras reservadas do Python (ex: if, else, for, while, def, class, etc.)
 
-nome = 'Bianca'
-idade = 37
-estado = 'Rio de Janeiro'
-profissao = 'Engenheira de Dados'
-tem_cachorro = True
 
 # Formatação de strings
 # f-strings é uma forma de formatar strings em Python.
@@ -31,12 +46,13 @@ tem_cachorro = True
 # Exemplo de formatação de strings usando f-string:
 # nome = 'Bianca'
 
-print(f'Nome: {nome}, Idade: {idade}, Estado: {estado}, Profissão: {profissao}, Tem cachorro: {tem_cachorro}')
+print(f'Nome: {nome}, Idade: {idade}, Altura: {altura}, Estado: {estado}, Profissão: {profissao}, Tem cachorro: {tem_cachorro}')
 
 # Verificação de tipos de variáveis
 
 print(type(nome))
 print(type(idade))
+print(type(altura))
 print(type(estado))
 print(type(profissao))
 print(type(tem_cachorro)) 
